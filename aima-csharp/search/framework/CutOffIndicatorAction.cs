61a6120011c9e69f488c83a423eae86f3e1bcfeb
ShobhitLamba <shobhitlamba.ynwa@gmail.com>
2016-06-15 20:51:16 +0530

    CutOffIndicatorAction added

---
+++ aima-csharp/search/framework/CutOffIndicatorAction.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using aima.core.agent.impl;
+
+namespace aima.core.search.framework
+{
+    /**
+ * A NoOp action that indicates a CutOff has occurred in a search. Used
+ * primarily by DepthLimited and IterativeDeepening search routines.
+ * 
+ * @author Ciaran O'Reilly
+ */
+ public class CutOffIndicatorAction : DynamicAction
+    {
+        public static readonly CutOffIndicatorAction CUT_OFF = new CutOffIndicatorAction();
+
+        // START-Action
+        public bool isNoOp()
+        {
+            return true;
+        }
+
+        // END-Action
+        private CutOffIndicatorAction(): base("CutOff")
+        {
+
+        }
+    }
+}
