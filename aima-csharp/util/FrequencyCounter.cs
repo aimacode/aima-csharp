4af12ed10bfe6c9717a9af7c9017aedc5fceb880
ShobhitLamba <shobhitlamba.ynwa@gmail.com>
2016-06-16 15:37:56 +0530

    Added FrequencyCounter and Triplet to utils

---
+++ aima-csharp/util/FrequencyCounter.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+
+namespace aima.core.util
+{
+    /**
+     * A utility class for keeping counts of objects. Will return 0 for any object
+     * for which it has not recorded a count against.
+     * 
+     * @author Ravi Mohan
+     * @author Mike Stampone
+     */
+     public class FrequencyCounter<T>
+    {
+        private Dictionary<T, int> counter;
+        private int total;
+
+        /**
+	     * Default Constructor.
+	     */
+        public FrequencyCounter()
+        {
+            counter = new Dictionary<T, int>();
+            total = 0;
+        }
+
+        /**
+	     * Returns the count to which the specified key is mapped in this frequency
+	     * counter, or 0 if the map contains no mapping for this key.
+	     * 
+	     * @param key
+	     *            the key whose associated count is to be returned.
+	     * 
+	     * @return the count to which this map maps the specified key, or 0 if the
+	     *         map contains no mapping for this key.
+	     */
+        public int getCount(T key)
+        {
+            int value = counter[key];
+            if (value == null)
+            {
+                return 0;
+            }
+            return value;
+        }
+
+        /**
+	     * Increments the count to which the specified key is mapped in this
+	     * frequency counter, or puts 1 if the map contains no mapping for this key.
+	     * 
+	     * @param key
+	     *            the key whose associated count is to be returned.
+	     */
+        public void incrementFor(T key)
+        {
+            int value = counter[key];
+            if(value == null)
+            {
+                counter.Add(key, 1);
+            }
+            else
+            {
+                counter.Add(key, value + 1);
+            }
+            // Keep track of the total
+            total++;
+        }
+
+        /**
+	     * Returns the count to which the specified key is mapped in this frequency
+	     * counter, divided by the total of all counts.
+	     * 
+	     * @param key
+	     *            the key whose associated count is to be divided.
+    	 * 
+    	 * @return the count to which this map maps the specified key, divided by
+    	 *         the total count.
+    	 */
+        public Double probabilityOf(T key)
+        {
+            int value = getCount(key);
+            if (value == 0)
+            {
+                return 0.0;
+            }
+            else
+            {
+                Double total = 0.0;
+                foreach (T k in counter.Keys)
+                {
+                    total += getCount(k);
+                }
+                return value / total;
+            }
+        }
+
+        /**
+	     * 
+	     * @return a set of objects for which frequency counts have been recorded.
+    	 */
+        public HashSet<T> getStates()
+        {
+            return new HashSet<T>(counter.Keys);
+        }
+
+        /**
+	     * Remove all the currently recorded frequency counts.
+	     */
+        public void clear()
+        {
+            counter.Clear();
+            total = 0;
+        }
+
+        public String toString()
+        {
+            return counter.ToString();
+        }
+    }
+}
---
+++ aima-csharp/util/Triplet.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+
+namespace aima.core.util
+{
+    /**
+     * @author Ravi Mohan
+     * @author Mike Stampone
+     * 
+     */
+     public class Triplet<X, Y, Z>
+    {
+        private X x;
+
+	    private Y y;
+
+	    private Z z;
+
+	    /**
+	     * Constructs a triplet with three specified elements.
+	     * 
+	     * @param x
+	     *            the first element of the triplet.
+	     * @param y
+	     *            the second element of the triplet.
+	     * @param z
+	     *            the third element of the triplet.
+	     */
+	    public Triplet(X x, Y y, Z z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        /**
+	     * Returns the second element of the triplet.
+	     * 
+	     * @return the second element of the triplet.
+	     */
+        public Y getSecond()
+        {
+            return y;
+        }
+
+        /**
+	     * Returns the third element of the triplet.
+	     * 
+	     * @return the third element of the triplet.
+	     */
+        public Z getThird()
+        {
+            return z;
+        }
+
+        public override bool Equals(Object o)
+        {
+            if (o is Triplet<X, Y, Z>)
+            {
+                Triplet<X, Y, Z> other = (Triplet<X, Y, Z>)o;
+                return (x.Equals(other.x)) && (y.Equals(other.y))
+                        && (y.Equals(other.y));
+            }
+            return false;
+        }
+
+        public int hashCode()
+        {
+            return x.GetHashCode() + 31 * y.GetHashCode() + 31 * z.GetHashCode();
+        }
+
+        public String toString()
+        {
+            return "< " + x.ToString() + " , " + y.ToString() + " , "
+                    + z.ToString() + " >";
+        }
+    }
+}
