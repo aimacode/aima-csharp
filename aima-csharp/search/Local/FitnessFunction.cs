2fc6aa4be5e7317418718d7523accfa41128a761
ShobhitLamba <shobhitlamba.ynwa@gmail.com>
2016-06-17 21:54:43 +0530

    Added FitnessFunction

---
+++ aima-csharp/search/Local/FitnessFunction.cs
@@ -0,0 +1,29 @@
+namespace aima.core.search.local
+{
+    /**
+     * Artificial Intelligence A Modern Approach (3rd Edition): page 127.<br>
+     * <br>
+     * Each state is rated by the objective function, or (in Genetic Algorithm
+     * terminology) the fitness function. A fitness function should return higher
+     * values for better states.
+     * <br>
+     * Here, we assume that all values are greater or equal to zero.
+     * 
+     * @author Ciaran O'Reilly
+     * 
+     * @param <A>
+     *            the type of the alphabet used in the representation of the
+     *            individuals in the population (this is to provide flexibility in
+     *            terms of how a problem can be encoded).
+     */
+    public interface FitnessFunction<A>
+    {
+        /**
+         * 
+         * @param individual
+         *            the individual whose fitness is to be accessed.
+         * @return the individual's fitness value (the higher the better).
+         */
+        double apply(Individual<A> individual);
+    }
+}
