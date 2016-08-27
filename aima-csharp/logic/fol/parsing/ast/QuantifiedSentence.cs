364bb112832b3ddab00b402eedde94f1201655c8
ShobhitLamba <shobhitlamba.ynwa@gmail.com>
2016-08-27 11:33:53 +0530

    Added QuantifiedSentence and TermEquality to parsing.ast

---
+++ aima-csharp/logic/fol/parsing/ast/QuantifiedSentence.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Collections.ObjectModel;
+using aima.core.logic.fol.parsing;
+
+namespace aima.core.logic.fol.parsing.ast
+{
+    /**
+     * @author Ravi Mohan
+     * @author Ciaran O'Reilly
+     */
+    public class QuantifiedSentence : Sentence
+    {
+	private String quantifier;
+	private List<Variable> variables = new List<Variable>();
+	private Sentence quantified;
+	private List<FOLNode> args = new List<FOLNode>();
+	private String stringRep = null;
+	private int hashCode = 0;
+
+	public QuantifiedSentence(String quantifier, List<Variable> variables,
+	       Sentence quantified)
+	{
+	    this.quantifier = quantifier;
+	    this.variables.AddRange(variables);
+	    this.quantified = quantified;
+	    this.args.AddRange(variables);
+	    this.args.Add(quantified);
+	}
+
+	public String getQuantifier()
+	{
+	    return quantifier;
+	}
+
+	public List<Variable> getVariables()
+	{
+	    return new ReadOnlyCollection<Variable>(variables).ToList<Variable>();
+	}
+
+	public Sentence getQuantified()
+	{
+	    return quantified;
+	}
+
+	// START-Sentence
+
+	public String getSymbolicName()
+	{
+	    return getQuantifier();
+	}
+
+	public bool isCompound()
+	{
+	    return true;
+	}
+
+	public List<FOLNode> getArgs()
+	{
+	    return new ReadOnlyCollection<FOLNode>(args).ToList<FOLNode>();
+	}
+
+	public Object accept(FOLVisitor v, Object arg)
+	{
+	    return v.visitQuantifiedSentence(this, arg);
+	}
+
+	public QuantifiedSentence copy()
+	{
+	    List<Variable> copyVars = new List<Variable>();
+	    foreach (Variable v in variables)
+	    {
+		copyVars.Add(v.copy());
+	    }
+	    return new QuantifiedSentence(quantifier, copyVars, quantified.copy());
+	}
+
+	// END-Sentence
+
+	public override bool Equals(Object o)
+	{
+
+	    if (this == o)
+	    {
+		return true;
+	    }
+	    if ((o == null) || !(o is QuantifiedSentence))
+	    {
+		return false;
+	    }
+	    QuantifiedSentence cs = (QuantifiedSentence)o;
+	    return cs.quantifier.Equals(quantifier)
+			    && cs.variables.Equals(variables)
+			    && cs.quantified.Equals(quantified);
+	}
+
+	public override int GetHashCode()
+	{
+	    if (0 == hashCode)
+	    {
+		hashCode = 17;
+		hashCode = 37 * hashCode + quantifier.GetHashCode();
+		foreach (Variable v in variables)
+		{
+		    hashCode = 37 * hashCode + v.GetHashCode();
+		}
+		hashCode = hashCode * 37 + quantified.GetHashCode();
+	    }
+	    return hashCode;
+	}
+
+	public override String ToString()
+	{
+	    if (null == stringRep)
+	    {
+		StringBuilder sb = new StringBuilder();
+		sb.Append(quantifier);
+		sb.Append(" ");
+		foreach (Variable v in variables)
+		{
+		    sb.Append(v.ToString());
+		    sb.Append(" ");
+		}
+		sb.Append(quantified.ToString());
+		stringRep = sb.ToString();
+	    }
+	    return stringRep;
+	}
+    }
+}
---
+++ aima-csharp/logic/fol/parsing/ast/TermEquality.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Collections.ObjectModel;
+using aima.core.logic.fol.parsing;
+
+namespace aima.core.logic.fol.parsing.ast
+{
+    /**
+     * @author Ravi Mohan
+     * @author Ciaran O'Reilly
+     */
+    public class TermEquality : AtomicSentence
+    {
+	private Term term1, term2;
+	private List<Term> terms = new List<Term>();
+	private String stringRep = null;
+	private int hashCode = 0;
+
+	public static String getEqualitySynbol()
+	{
+	    return "=";
+	}
+
+	public TermEquality(Term term1, Term term2)
+	{
+	    this.term1 = term1;
+	    this.term2 = term2;
+	    terms.Add(term1);
+	    terms.Add(term2);
+	}
+
+	public Term getTerm1()
+	{
+	    return term1;
+	}
+
+	public Term getTerm2()
+	{
+	    return term2;
+	}
+		
+	// START-AtomicSentence
+
+	public String getSymbolicName()
+	{
+	    return getEqualitySynbol();
+	}
+
+	public bool isCompound()
+	{
+	    return true;
+	}
+
+	public List<FOLNode> getArgs()
+	{
+	    return new ReadOnlyCollection<Term>(terms).ToList<FOLNode>();
+	}
+
+	public Object accept(FOLVisitor v, Object arg)
+	{
+	    return v.visitTermEquality(this, arg);
+	}
+
+	public FOLNode copy()
+	{
+	    return new TermEquality((Term)term1.copy(), (Term)term2.copy());
+	}
+
+	// END-AtomicSentence
+	
+	public override bool Equals(Object o)
+	{
+
+	    if (this == o)
+	    {
+		return true;
+	    }
+	    if ((o == null) || !(o is TermEquality))
+	    {
+		return false;
+	    }
+	    TermEquality te = (TermEquality)o;
+
+	    return te.getTerm1().Equals(term1) && te.getTerm2().Equals(term2);
+	}
+
+	public override int GetHashCode()
+	{
+
+	    if (0 == hashCode)
+	    {
+		hashCode = 17;
+		hashCode = 37 * hashCode + getTerm1().GetHashCode();
+		hashCode = 37 * hashCode + getTerm2().GetHashCode();
+	    }
+	    return hashCode;
+	}
+
+	public override String ToString()
+	{
+	    if (null == stringRep)
+	    {
+		StringBuilder sb = new StringBuilder();
+		sb.Append(term1.ToString());
+		sb.Append(" = ");
+		sb.Append(term2.ToString());
+		stringRep = sb.ToString();
+	    }
+	    return stringRep;
+	}
+    }
+}
