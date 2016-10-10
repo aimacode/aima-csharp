313dd5f943e1dcad54ec2d6e559c3bbe7fb3c249
ShobhitLamba <shobhitlamba.ynwa@gmail.com>
2016-10-10 19:02:02 +0530

    Work started on inference.proof

---
+++ aima-csharp/logic/fol/inference/proof/Proof.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using aima.core.logic.fol.parsing.ast;
+
+namespace aima.core.logic.fol.inference.proof
+{
+    /**
+     * @author Ciaran O'Reilly
+     * 
+     */
+    public interface Proof
+    {
+	/**
+	 * 
+	 * @return A list of proof steps that show how an answer was derived.
+	 */
+	List<ProofStep> getSteps();
+
+	/**
+	 * 
+	 * @return a Map of bindings for any variables that were in the original
+	 *         query. Will be an empty Map if no variables existed in the
+	 *         original query.
+	 */
+	Dictionary<Variable, Term> getAnswerBindings();
+
+	/**
+	 * 
+	 * @param updatedBindings
+	 *            allows for the bindings to be renamed. Note: should not be
+	 *            used for any other reason.
+	 */
+	void replaceAnswerBindings(Dictionary<Variable, Term> updatedBindings);
+    }
+}
---
+++ aima-csharp/logic/fol/inference/proof/ProofStep.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+namespace aima.core.logic.fol.inference.proof
+{
+    /**
+     * @author Ciaran O'Reilly
+     * 
+     */
+    public interface ProofStep
+    {
+	int getStepNumber();
+
+	void setStepNumber(int step);
+
+	List<ProofStep> getPredecessorSteps();
+
+	String getProof();
+
+	String getJustification();
+    }
+}
